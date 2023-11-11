f1ac1b668fd821f8d6c6d6e85affe32501b6ce9a
Alice-space <alicespace.li@outlook.com>
2023-11-11 20:05:26 +0800

    fix: use pebble to generate

--- imageexport.py
+++ imageexport.py
@@ -1,6 +1,6 @@
-import multiprocessing
 import os
 import subprocess
+import pebble
 
 from qgiscontroller import export_image
 from logger import configure_logger
@@ -89,81 +89,95 @@ def gdal_translate(heightimage_output_folder: str, tile: str,
         capture_output=True, text=True
     )
     logger.info(f"gdal_translate {tile} stdout: {result.stdout}")
-    logger.error(f"gdal_translate {tile} stderr: {result.stderr}")
+    if result.stderr:
+        logger.error(f"gdal_translate {tile} stderr: {result.stderr}")
 
 
-def imageExportTile(pool, blocks_per_tile: int,
+def imageExportTile(pool: pebble.ProcessPool,
+                    futures: dict[pebble.ProcessFuture, tuple],
+                    blocks_per_tile: int,
                     degree_per_tile: int, xMin: float, yMin: float):
     xMax = xMin + degree_per_tile
     yMax = yMin + degree_per_tile
-
     tile = calculateTiles(xMin, yMax)
-    all_generated = True
 
     tiles_folder = os.path.join(image_output_folder, f'{tile}/')
     if not os.path.exists(tiles_folder):
         os.makedirs(tiles_folder)
 
     logger.info(f"generating images of {tile}...")
-    all_output_files_exist = all(os.path.exists(os.path.join(
-        tiles_folder, f"{tile}_{name}.png")) for name in LAYER_NAMES)
-    if not all_output_files_exist:
-        all_generated = False
-        pool.apply_async(export_image,
-                         (CONFIG['qgis_project_path'],
-                          blocks_per_tile,
-                          xMin, xMax, yMin, yMax, tile,
-                          LAYER_NAMES, tiles_folder))
+
+    if not all(os.path.exists(os.path.join(
+            tiles_folder, f"{tile}_{name}.png"))
+            for name in LAYER_NAMES):
+        f = pool.schedule(
+            export_image, (CONFIG['qgis_project_path'],
+                           blocks_per_tile,
+                           xMin, xMax, yMin, yMax, tile,
+                           LAYER_NAMES, tiles_folder))
+        futures[f] = (export_image, (CONFIG['qgis_project_path'],
+                      blocks_per_tile,
+                      xMin, xMax, yMin, yMax, tile,
+                      LAYER_NAMES.copy(), tiles_folder))
     else:
         logger.info(f"Skipping qgis_project_path of {tile}")
-    all_output_files_exist = all(os.path.exists(os.path.join(
+
+    if not all(os.path.exists(os.path.join(
         tiles_folder, f"{tile}_{name}.png")
-    ) for name in BATHYMETRY_LAYER_NAMES)
-    if not all_output_files_exist:
-        all_generated = False
-        pool.apply_async(export_image,
-                         (CONFIG['qgis_bathymetry_project_path'],
-                          blocks_per_tile, xMin, xMax, yMin, yMax, tile,
-                          BATHYMETRY_LAYER_NAMES, tiles_folder))
+    ) for name in BATHYMETRY_LAYER_NAMES):
+        f = pool.schedule(
+            export_image, (CONFIG['qgis_bathymetry_project_path'],
+                           blocks_per_tile, xMin, xMax, yMin, yMax, tile,
+                           BATHYMETRY_LAYER_NAMES, tiles_folder))
+        futures[f] = (export_image, (CONFIG['qgis_bathymetry_project_path'],
+                      blocks_per_tile,
+                      xMin, xMax, yMin, yMax, tile,
+                      BATHYMETRY_LAYER_NAMES.copy(), tiles_folder))
     else:
         logger.info(f"Skipping qgis_bathymetry_project_path of {tile}")
-    all_output_files_exist = all(os.path.exists(os.path.join(
+
+    if not all(os.path.exists(os.path.join(
         tiles_folder, f"{tile}_{name}.png")
-    ) for name in TERRAIN_LAYER_NAMES)
-    if not all_output_files_exist:
-        all_generated = False
-        pool.apply_async(export_image,
-                         (CONFIG['qgis_terrain_project_path'],
-                          blocks_per_tile, xMin, xMax, yMin, yMax, tile,
-                          TERRAIN_LAYER_NAMES, tiles_folder))
+    ) for name in TERRAIN_LAYER_NAMES):
+        f = pool.schedule(
+            export_image, (CONFIG['qgis_terrain_project_path'],
+                           blocks_per_tile, xMin, xMax, yMin, yMax, tile,
+                           TERRAIN_LAYER_NAMES, tiles_folder))
+        futures[f] = (export_image, (CONFIG['qgis_terrain_project_path'],
+                      blocks_per_tile,
+                      xMin, xMax, yMin, yMax, tile,
+                      TERRAIN_LAYER_NAMES.copy(), tiles_folder))
     else:
         logger.info(f"Skipping qgis_terrain_project_path of {tile}")
-    all_output_files_exist = os.path.exists(os.path.join(
-        tiles_folder, f"{tile}.png"))
-    if not all_output_files_exist:
-        all_generated = False
-        pool.apply_async(export_image,
-                         (CONFIG['qgis_heightmap_project_path'],
-                          blocks_per_tile, xMin, xMax, yMin, yMax, tile,
-                          HEIGHT_LAYER_NAMES, tiles_folder))
+
+    if not os.path.exists(os.path.join(
+            tiles_folder, f"{tile}.png")):
+        f = pool.schedule(
+            export_image, (CONFIG['qgis_heightmap_project_path'],
+                           blocks_per_tile, xMin, xMax, yMin, yMax, tile,
+                           HEIGHT_LAYER_NAMES, tiles_folder))
+        futures[f] = (export_image, (CONFIG['qgis_heightmap_project_path'],
+                      blocks_per_tile,
+                      xMin, xMax, yMin, yMax, tile,
+                      HEIGHT_LAYER_NAMES.copy(), tiles_folder))
     else:
         logger.info(f"Skipping qgis_heightmap_project_path of {tile}")
 
     heightmap_output_folder = os.path.join(tiles_folder, 'heightmap')
     if not os.path.exists(heightmap_output_folder):
         os.makedirs(heightmap_output_folder)
-    all_output_files_exist = os.path.exists(os.path.join(
-        heightmap_output_folder, f"{tile}_exported.png"))
-    if not all_output_files_exist:
-        all_generated = False
-        pool.apply_async(gdal_translate,
-                         (heightmap_output_folder, tile,
-                          blocks_per_tile, xMin, xMax,
-                          yMin, yMax))
+
+    if not os.path.exists(os.path.join(
+            heightmap_output_folder, f"{tile}_exported.png")):
+        f = pool.schedule(
+            gdal_translate, (heightmap_output_folder, tile,
+                             blocks_per_tile, xMin, xMax,
+                             yMin, yMax))
+        futures[f] = (gdal_translate, (heightmap_output_folder, tile,
+                                       blocks_per_tile, xMin, xMax,
+                                       yMin, yMax))
     else:
         logger.info(f"Skipping gdal_translate of {tile}")
-
-    return all_generated
 
 
 def imageExport():
@@ -175,18 +189,36 @@ def imageExport():
     degree_per_tile = 2
     blocks_per_tile = 512
     # x -180 ~ 180  y -90 ~ 90
-    all_generated = False
-    while not all_generated:
-        all_generated = True
-        logger.info("image export...")
-        pool = multiprocessing.Pool(processes=CONFIG['threads'])
-        for xMin in range(-180, 180, degree_per_tile):
-            for yMin in range(-90, 90, degree_per_tile):
-                g = imageExportTile(
-                    pool, blocks_per_tile, degree_per_tile, xMin, yMin)
-                if not g:
-                    all_generated = False
-        pool.close()
-        pool.join()
+    logger.info("image export...")
+    pool = pebble.ProcessPool(max_workers=CONFIG["threads"], max_tasks=1)
+    futures: dict[pebble.ProcessFuture, tuple] = {}
+
+    for xMin in range(-180, 180, degree_per_tile):
+        for yMin in range(-90, 90, degree_per_tile):
+            imageExportTile(pool, futures, blocks_per_tile,
+                            degree_per_tile, xMin, yMin)
+
+    while futures:
+        for f in list(futures.keys()):
+            if f.done():
+                (func, args) = futures[f]
+                if func == export_image:
+                    tile = args[6]
+                    qgis_project_path = args[0]
+                else:
+                    tile = args[1]
+                    qgis_project_path = ''
+                try:
+                    _ = f.result()
+                    logger.info(f"{func} {tile} finished!")
+                    # Remove the task from the dict once it's completed
+                    del futures[f]
+                except Exception as e:
+                    logger.error(f'error on {tile}: {e}')
+                    logger.info(
+                        f"Resubmitting {func} {tile} {qgis_project_path}")
+                    fn = pool.schedule(func, args)
+                    futures[fn] = (func, args)
+                    # Remove the broken task from the dict
+                    del futures[f]
     logger.info("image export done")
-    # examine the output make sure all the tiles are generated
